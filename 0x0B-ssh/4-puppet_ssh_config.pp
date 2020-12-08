d66d9929d0ccc6616332116c1a96ab4a71a98cb5
Sebastian_Orozco <omsebas@gmail.com>
2020-12-08 22:23:04 +0000

    Task 4

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,18 @@
+#puppet script to modify the ssh_confih archive
+
+include stdlib
+file_line {'config1':
+  path => '/etc/ssh/ssh_config',
+  line => '    PubkeyAuthentication yes',
+  match => '^*PubkeyAuthentication yes*'
+}
+file_line {'config2':
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/holberton',
+  match => '^*IdentityFile ~/.ssh/holberton*'
+}
+file_line {'config3':
+  path => '/etc/ssh/ssh_config',
+  line => '    KbdInteractiveAuthentication no',
+  match => '^*KbdInteractiveAuthentication no*'
+}
