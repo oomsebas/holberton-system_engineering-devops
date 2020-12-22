ede6882b28e5c65a71752cde8891fb377ce6a3e1
Sebastian_Orozco <omsebas@gmail.com>
2020-12-22 04:43:22 +0000

    Task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,8 +2,34 @@
 
 include stdlib
 
+exec {'install server 1':
+     command => 'sudo apt-get -y update',
+     path    => '/usr/bin'
+      }
+exec {'installs server 2':
+     command => 'sudo apt-get upgrade',
+     path    => '/usr/bin'
+      }
+exec {'installs server 3':
+     command => 'sudo apt-get install -y nginx',
+     path    => '/usr/bin'
+      }
+exec {'install server 4':
+     command => 'sudo ufw allow "Nginx HTTP"',
+     path    => '/usr/bin'
+      }
 file_line {'config2':
   path  => '/etc/nginx/nginx.conf',
   after => '^http*{*',
   line  => '        add_header X-Server-By $HOSTNAME;',
 }
+
+exec {'install server 5':
+     command => '/bin/echo "Holberton School" | sudo tee /usr/share/nginx/html/index.html',
+     path    => '/usr/bin:/usr/sbin:/bin'
+      }
+
+exec {'install server 6':
+     command => 'sudo service nginx restart',
+     path    => '/usr/bin'
+      }
