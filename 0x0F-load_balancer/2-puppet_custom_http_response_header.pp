89ebe7af3b58c47d39a178fb313afdfead726660
Sebastian_Orozco <omsebas@gmail.com>
2020-12-22 04:56:36 +0000

    task2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,35 +1,10 @@
 #puppet script to modify nginx.config
 
-include stdlib
 
-exec {'install server 1':
-     command => 'sudo apt-get -y update',
-     path    => '/usr/bin'
-      }
-exec {'installs server 2':
-     command => 'sudo apt-get upgrade',
-     path    => '/usr/bin'
-      }
-exec {'installs server 3':
-     command => 'sudo apt-get install -y nginx',
-     path    => '/usr/bin'
-      }
-exec {'install server 4':
-     command => 'sudo ufw allow "Nginx HTTP"',
-     path    => '/usr/bin'
-      }
-file_line {'config2':
-  path  => '/etc/nginx/nginx.conf',
-  after => '^http*{*',
-  line  => '        add_header X-Server-By $HOSTNAME;',
-}
-
-exec {'install server 5':
-     command => '/bin/echo "Holberton School" | sudo tee /usr/share/nginx/html/index.html',
-     path    => '/usr/bin:/usr/sbin:/bin'
-      }
-
-exec {'install server 6':
-     command => 'sudo service nginx restart',
-     path    => '/usr/bin'
+exec {'install server':
+      command  => 'sudo apt-get update;
+      sudo apt-get -y install nginx;
+      sudo sed -i "21 i\\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf;
+      sudo service nginx restart',
+      provider => 'shell',
       }
