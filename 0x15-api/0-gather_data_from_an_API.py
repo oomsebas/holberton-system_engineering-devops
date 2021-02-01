b1f60ae21462e68cbfe0a02beacb5c85ace75959
Sebastian_Orozco <omsebas@gmail.com>
2021-02-01 20:50:56 +0000

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,25 +10,26 @@ Attributes:
 import requests
 from sys import argv
 
-# querys to bring the data of the tasks and employee name
-todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
-employee = requests.get('https://jsonplaceholder.typicode.com/users/' +
-                        argv[1])
+if __name__ == '__main__':
+    # querys to bring the data of the tasks and employee name
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    employee = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                            argv[1])
 
-# convert the text response to json format
-employee_dict = employee.json()
-todos_dict = todos.json()
+    # convert the text response to json format
+    employee_dict = employee.json()
+    todos_dict = todos.json()
 
-# get the nedded data
-userName = employee_dict['name']
-body = [items["title"] for items in todos_dict
-        if items["userId"] == int(argv[1]) and
-        items["completed"] is True]
-ready = [items["completed"] for items in todos_dict
-         if items["userId"] == int(argv[1])]
+    # get the nedded data
+    userName = employee_dict.get('name')
+    body = [items.get("title") for items in todos_dict
+            if items.get("userId") == int(argv[1]) and
+            items.get("completed") is True]
+    ready = [items.get("completed") for items in todos_dict
+             if items.get("userId") == int(argv[1])]
 
-# print the results
-print('Employee {} is done with tasks({}/{})'
-      .format(userName, sum(ready), len(ready)))
-for message in body:
-    print('\t {}'.format(message))
+    # print the results
+    print('Employee {} is done with tasks({}/{}):'
+          .format(userName, sum(ready), len(ready)))
+    for message in body:
+        print('\t {}'.format(message))
