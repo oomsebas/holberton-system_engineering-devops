be605e56194243400553b0ee6c39a5d9a461936b
Sebastian_Orozco <omsebas@gmail.com>
2021-02-05 03:50:00 +0000

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,15 +5,18 @@ listed for a given subreddit.
 
 
 def top_ten(subreddit):
-    """ query the 10 hot post """
+    """
+    query the 10 hot post from reddit according to argv
+    """
     import requests
 
     hot_pst = requests.get('https://www.reddit.com/r/{}/hot.json?limit=10'
                            .format(subreddit),
                            headers={"User-Agent": "My-User-Agent"},
                            allow_redirects=False)
+    hot_pst_json = hot_pst.json()
     if hot_pst.status_code >= 300:
-        print ('None')
+        print('None')
     else:
         [print(post.get("data").get("title"))
-         for post in hot_pst.json().get("data").get("children")]
+         for post in hot_pst_json.get("data").get("children")]
