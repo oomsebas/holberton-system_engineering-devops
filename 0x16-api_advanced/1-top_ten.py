4c88c5128fde1a6aa9a3c0b3756fd4360ae8380a
Sebastian_Orozco <omsebas@gmail.com>
2021-02-05 03:35:55 +0000

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" hat queries the Reddit API and prints the titles of the first 10 hot posts
+listed for a given subreddit.
+"""
+
+
+def top_ten(subreddit):
+    """ query the 10 hot post """
+    import requests
+
+    hot_pst = requests.get('https://www.reddit.com/r/{}/hot.json?limit=10'
+                           .format(subreddit),
+                           headers={"User-Agent": "My-User-Agent"},
+                           allow_redirects=False)
+    if hot_pst.status_code >= 300:
+        return None
+    else:
+        [print(post.get("data").get("title"))
+         for post in hot_pst.json().get("data").get("children")]
