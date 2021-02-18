c9f4411989b785d7947fcb21e625a528b2aaff7c
Sebastian_Orozco <omsebas@gmail.com>
2021-02-18 22:14:42 +0000

    task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fix error server 500 on apache for a typo en wp-file settings
+exec { 'Fix server':
+     command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+     path    => '/bin/'
+      }	    
