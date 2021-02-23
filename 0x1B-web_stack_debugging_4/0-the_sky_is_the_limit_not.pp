b52d3fede9f55aaf6b6434559038ab6c645c2e10
omsebas <omsebas@gmail.com>
2021-02-23 12:15:05 -0500

    Task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+#extend the limit of open files in the system
+exec { 'change max of opened files':
+      command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 2000\"/g"
+       /etc/default/nginx; service nginx restart',
+      path    => ['/usr/bin', '/bin']
+}
+
